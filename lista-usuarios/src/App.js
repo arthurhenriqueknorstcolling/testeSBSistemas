87e426e971a7c3d44e0d905795a14ef73b29fc21
arthurhenriqueknorstcolling <112160751+arthurhenriqueknorstcolling@users.noreply.github.com>
2022-08-26 06:19:52 -0300

    Update App.js

--- lista-usuarios/src/App.js
+++ lista-usuarios/src/App.js
@@ -5,20 +5,33 @@ import Footer from "./UI/Footer";
 import Header from "./UI/Header";
 import ModalComponent from "./UI/Modal";
 import Users from "./components/Users";
+import { DefaultButton } from "./UI/Buttons";
 
 function App() {
   //retorna a aplicação com cabeçalho, lista de usuários e rodapé
+  //o conteúdo do cabeçalho são dois botões, um abre o modal sobre o projeto e o outro leva a fonte dos dados renderizados
   return (
     <div>
       <Header
         content={
-          <ModalComponent
-            className={"btn btn-outline-dark btn-lg"}
-            title={"Especificações do Projeto"}
-            buttonTittle={"About"}
-          >
-            <About />
-          </ModalComponent>
+          <>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://jsonplaceholder.typicode.com/users"
+            >
+              <DefaultButton className={"btn btn-light btn-lg"}>
+                Database
+              </DefaultButton>
+            </a>
+            <ModalComponent
+              className={"btn btn-light btn-lg"}
+              title={"Especificações do Projeto"}
+              buttonTittle={"About"}
+            >
+              <About />
+            </ModalComponent>
+          </>
         }
       >
         Users List
